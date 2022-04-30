0d92370fc5ad0ba4b0ac52f9d8300078747f1c83
kidow <wcgo2ling@gmail.com>
2022-04-30 15:37:11 +0900

    logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image'
+
 interface State {}
 
 const Home = () => {
-  return <div className="container mx-auto">Home</div>
+  return (
+    <div className="container mx-auto px-6 pt-10 pb-20">
+      <div>
+        <img
+          src="/kidow-analytics.svg"
+          draggable={false}
+          alt=""
+          className="h-7"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Home
